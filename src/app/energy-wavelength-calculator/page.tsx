4448f5965f94a6ac6e0c963b0c0deda1a7296c1a
Kelvin Aalto <kelvin.ho@aalto.fi>
2026-04-10 17:34:59 +0300

    again

--- src/app/energy-wavelength-calculator/page.tsx
+++ src/app/energy-wavelength-calculator/page.tsx
@@ -2,11 +2,22 @@
 import React, { useState } from "react";
 
 export default function EnergyWavelengthCalculator() {
-  const electronVolt = 1.602176634e-19;
+  const electronVolt = 1.602176634e-19; // J/eV
   const c = 299792458;
   const planck = 6.62607015e-34;
   const [energy, setEnergy] = useState(0);
   const [wvlen, setWvlen] = useState(0);
+
+  function eVtoNm(eV: number): number {
+    const joules = eV * electronVolt;
+    return (planck * c) / joules;
+  }
+
+  function nMtoeV(nm: number): number {
+    const wvlen = nm * 1e-9;
+    return (electronVolt * (planck * c)) / wvlen;
+  }
+
   return (
     <>
       <h1>Energy Wavelength Calculator</h1>
